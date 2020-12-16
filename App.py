800df0998e6708eeffcfceacaf0997fbbfc5bee2
WhitneyDluhosh <whitneydluhosh@gmail.com>
2020-12-16 13:19:28 -0800

    Add files via upload
    Chat API Codebase

---
+++ App.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask  # import flask
+from chatapp import *
+# !/usr/bin/python
+import train_chatbot
+
+app = Flask(__name__)  # create an app instance
+
+
+@app.route("/<response>")               # at the end point
+def create_response(response):          # call method to find response
+    return chatbot_response(response)   # Get response using flask
+
+if __name__ == "__main__":              # on running python app.py
+    app.run(debug=True)                 # run the flask app
