74bb9833573f994158578e9792fe40ed286e3149
Dluhosh <Whitney.Dluhosh@gov.bc.ca>
2022-03-23 14:59:44 -0700

    update readme based on en/fr changes

--- train_chatbot.py
+++ train_chatbot.py
@@ -73,7 +73,6 @@ training = np.array(training)
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
 print("Training data created")
-
 # Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
 # equal to number of intents to predict output intent with softmax
 model = Sequential()
