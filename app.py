2626e210fcc01737b4935d17fd41e705e9ff32d7
Dluhosh <Whitney.Dluhosh@gov.bc.ca>
2022-03-24 11:15:10 -0700

    maintain old and new route so there is no errors with olderversion of the app requests

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ app = Flask(__name__)  # create an app instance
 def create_response(lang, response):          # call method to find response
     return chatbot_response(lang, response)   # Get response using flask
 
+@app.route("/<response>")               # at the end point
+def create_responses(response):          # call method to find response
+    return chatbot_response("en", response)   # Get response using flask
+
 if __name__ == "__main__":              # on running python app.py
     from waitress import serve
     serve(app, host=SERVER_NAME, port=5000)
